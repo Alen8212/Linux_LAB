18a80b92be4e35dd7de623576e552e23f178f38f
Alen George <93701441+Alen8212@users.noreply.github.com>
2022-05-31 14:33:31 +0530

    2nd commit

---
+++ palindrom.sh
@@ -0,0 +1,21 @@
+echo "enter a number"
+read a
+sum=0
+rev ""
+g= $a
+while [$a -gt 0]
+do
+
+sum = $(($a %  10))
+a =$(( $n / 10))
+rev=$( echo ${rev}${sum} )
+done
+
+if [$g -eq $rev]
+then
+echo "palindrome"
+else
+echo "not palindrome"
+fi
+
+
